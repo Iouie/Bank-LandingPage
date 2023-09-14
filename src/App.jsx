912baec8d46658993e041a0b21ef84ea465dbf13
Louie <sxl7953@rit.edu>
2023-09-13 21:59:31 -0400

    finished nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <div className="text-4xl">HI</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;

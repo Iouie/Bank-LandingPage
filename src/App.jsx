e9ab61a312e2a8eaa5acdaf0869a438050393e3c
Louie <sxl7953@rit.edu>
2023-10-10 21:10:23 -0400

    finished mobile

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,20 @@ import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import LatestArticle from "./components/LatestArticle";
 import FooterComponent from "./components/FooterComponent";
+import MobileComponent from "./components/MobileComponent";
+import { useState, useEffect } from "react";
 
 function App() {
   return (
-    <div className="bg-[#fafafa] overflow-hidden">
-      <Navbar />
-      <Main />
-      <LatestArticle />
-      <FooterComponent />
-    </div>
+    <>
+      {/* <div className="bg-[#fafafa] overflow-hidden">
+        <Navbar />
+        <Main />
+        <LatestArticle />
+        <FooterComponent />
+      </div> */}
+      <MobileComponent />
+    </>
   );
 }
 
